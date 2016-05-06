1339a790f02c16f079a987d1a618e8f870f41b53
Konboi <ryosuke.yabuki@gmail.com>
2016-05-06 11:51:24 +0900

    postとcontentのparse部分を作成
    同じロジックがあるのでまとめてもいいかもしれない

--- post.go
+++ post.go
@@ -9,19 +9,20 @@ import (
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/pkg/errors"
 	"github.com/russross/blackfriday"
+	"gopkg.in/yaml.v2"
 )
 
+type PostHeader struct {
+	Title string   `yaml:"title"`
+	Tag   []string `yaml:"tag"`
+}
+
 type Post struct {
-	Title   string
+	Header  *PostHeader
 	Content template.HTML
 }
 
 type Posts []Post
-
-type PostHeader struct {
-	Title string `yaml:"title"`
-	Tag   string `yaml:"tag"`
-}
 
 var (
 	headerSeparatorStr = `---\n+`
@@ -35,9 +36,22 @@ func parseHeader(contentStr string) (*PostHeader, error) {
 		return nil, fmt.Errorf("header format is invalid")
 	}
 
-	postHeader := &{}
+	postHeader := &PostHeader{}
+	if err := yaml.Unmarshal([]byte(header[1]), postHeader); err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	return postHeader, nil
+}
+
+func parseContent(contentStr string) (string, error) {
+	content := headerSeparator.Split(contentStr, 3)
+
+	if len(content) != 3 || content[0] != "" {
+		return "", fmt.Errorf("content format is invalid")
+	}
+
+	return content[2], nil
 }
 
 func NewPost(path string) (*Post, error) {
@@ -48,18 +62,23 @@ func NewPost(path string) (*Post, error) {
 	}
 
 	contentStr := string(postFile)
-	md := blackfriday.MarkdownCommon(postFile)
 
-	_, err = parseHeader(contentStr)
-
+	header, err := parseHeader(contentStr)
 	if err != nil {
 		return nil, errors.Wrap(err, "fail parse header")
 	}
 
+	parseContentStr, err := parseContent(contentStr)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "fail parse content")
+	}
+
+	md := blackfriday.MarkdownCommon([]byte(parseContentStr))
 	content := bluemonday.UGCPolicy().Sanitize(string(md))
 
 	post := &Post{
-		Title:   "",
+		Header:  header,
 		Content: template.HTML(content),
 	}
 
