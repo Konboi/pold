5ee7c6c5f7ccdfa27abbee7584317fdbbe04295e
Konboi <ryosuke.yabuki@gmail.com>
2016-09-22 13:06:13 +0900

    Add atom.xml feed

--- server.go
+++ server.go
@@ -36,6 +36,7 @@ var (
 	root, _        = os.Getwd() // todo set config
 	topPostNum     = 10         // TODO: set config
 	archivePostNum = 9999
+	atomFeedNum    = 10
 )
 
 func NewServer(conf Config) (server *Server) {
@@ -57,6 +58,7 @@ func (s *Server) Run() {
 
 	router := httprouter.New()
 	router.GET("/", IndexHandler)
+	router.GET("/atom.xml", AtomHandler)
 	router.GET("/post/*path", PostHandler)
 	router.GET("/archive", ArchiveHandler)
 
@@ -136,6 +138,28 @@ func ArchiveHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params)
 	}
 
 	jt, err := jetSet.GetTemplate("archive.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := jt.Execute(w, nil, view); err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func AtomHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	posts, err := PublishedPosts(atomFeedNum)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	view := &View{
+		Blog:  blog,
+		Posts: posts,
+	}
+
+	w.Header().Set("Content-Type", "application/atom+xml")
+	jt, err := jetSet.GetTemplate("atom.xml")
 	if err != nil {
 		log.Fatal(err)
 	}
