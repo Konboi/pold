06b30c7578904dd64da5ea15bb76a52139bafb75
Konboi <ryosuke.yabuki@gmail.com>
2017-04-21 15:13:28 +0900

    Add api handler

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package pold
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
@@ -29,6 +31,10 @@ type View struct {
 	Posts   Posts
 	Content template.HTML
 	Tag     string
+}
+
+type API struct {
+	Results Posts `json:"results"`
 }
 
 var (
@@ -63,6 +69,10 @@ func (s *Server) Run() {
 	router.GET("/tag/*tag", TagHandler)
 	router.GET("/archive", ArchiveHandler)
 	router.GET("/atom.xml", AtomHandler)
+	router.GET("/api/index", IndexAPIHandler)
+	router.GET("/api/post/*path", PostAPIHandler)
+	router.GET("/api/tag/*tag", TagAPIHandler)
+	router.GET("/api/archive", ArchiveAPIHandler)
 
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(s.conf.Port), router))
 }
@@ -191,11 +201,93 @@ func AtomHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "application/atom+xml")
 	jt, err := jetSet.GetTemplate("atom.xml")
 	if err != nil {
-		log.Fatal(err)
+		log.Println("error get template", err.Error())
+		return
 	}
 	if err := jt.Execute(w, nil, view); err != nil {
 		log.Println(err.Error())
 	}
+}
+
+func IndexAPIHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	posts, err := PublishedPosts(archivePostNum)
+
+	if err != nil {
+		log.Println("error get published posts", err.Error())
+		notFound(w)
+		return
+	}
+
+	result := &API{Results: posts}
+	if err := writeJSON(w, result); err != nil {
+		log.Println("error write json", err.Error())
+	}
+	return
+}
+
+func PostAPIHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	postFilePath := strings.Replace(ps.ByName("path"), ".html", "", -1)
+	post, err := NewPost(postFilePath)
+	if err != nil {
+		log.Println("error get new post", err.Error())
+		notFound(w)
+		return
+	}
+
+	result := &API{Results: Posts{post}}
+	if err := writeJSON(w, result); err != nil {
+		log.Println("error write json", err.Error())
+		notFound(w)
+		return
+	}
+
+	return
+}
+
+func TagAPIHandler(w http.ResponseWriter, r *http.Request, tag httprouter.Params) {
+	tagName := strings.Replace(tag.ByName("tag"), "/", "", -1)
+	tagName = strings.Replace(tagName, ".html", "", -1)
+
+	posts, err := PublishedPostsByTagName(tagName)
+	if err != nil {
+		log.Println("not tag post", err.Error())
+		notFound(w)
+		return
+	}
+	if len(posts) == 0 {
+		notFound(w)
+		return
+	}
+
+	result := &API{Results: posts}
+	if err := writeJSON(w, result); err != nil {
+		log.Println("error write json", err.Error())
+	}
+	return
+}
+
+func ArchiveAPIHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	posts, err := PublishedPosts(archivePostNum)
+	if err != nil {
+		log.Println("error get publied post", err.Error())
+		return
+	}
+	result := &API{Results: posts}
+	if err := writeJSON(w, result); err != nil {
+		log.Println("error write json", err.Error())
+	}
+	return
+}
+
+func writeJSON(w http.ResponseWriter, result interface{}) error {
+	body := &bytes.Buffer{}
+	if err := json.NewEncoder(body).Encode(result); err != nil {
+		log.Println("error json marshal", err.Error())
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(body.Bytes())
+	return nil
 }
 
 func notFound(w http.ResponseWriter) {
